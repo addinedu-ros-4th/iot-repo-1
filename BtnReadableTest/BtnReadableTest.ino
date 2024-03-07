0dee3e3b4de452d931f42712692293c1d7baee45
yhryu96 <yhryu96@gmail.com>
2024-03-07 18:42:00 +0900

    test over : Serial can only work one way

--- BtnReadableTest/BtnReadableTest.ino
+++ BtnReadableTest/BtnReadableTest.ino
@@ -1,5 +1,8 @@
+const int LED_PIN = 13;
+
 void setup() {
     Serial.begin(9600);
+    pinMode(LED_PIN, OUTPUT);
 }
 
 void loop() {
@@ -7,5 +10,11 @@ void loop() {
         String received = Serial.readStringUntil('\n');
         Serial.print("Received: ");
         Serial.println(received);
+
+        // "button_pressed" 메시지를 받으면 LED를 켭니다.
+        if (received.equals("button_pressed")) {
+            digitalWrite(LED_PIN, HIGH);
+            Serial.println("LED on");
+        }
     }
 }
--- IotProject.py
+++ IotProject.py
@@ -6,52 +6,40 @@ from PyQt5.QtSerialPort import QSerialPort, QSerialPortInfo
 from PyQt5.QtCore import QIODevice  # QIODevice를 import 추가
 from PyQt5.QtWidgets import QApplication, QMainWindow
 import serial
-
-# 나머지 코드 생략
-
+import time
 
 from_class = uic.loadUiType("/home/ryu/amr_ws/arduino/iot-repo-1/IotProject.ui")[0]
 
-# class Camera(QThread):
-#     update = pyqtSignal()
-    
-#     def __init__(self, sec=0, parent = None) :
-#         super().__init__()
-#         self.main = parent
-#         self.running = True
 
-#     def run(self) :
-
-#         while self.running == True :
-#             self.update.emit()
-#             time.sleep(0.1)
-
-#     def stop(self) :
-#         self.running = False
-
-class WindowClass(QMainWindow, from_class) :
-    def __init__(self) :
+class WindowClass(QMainWindow, from_class):
+    def __init__(self):
         super().__init__()
         self.setupUi(self)
+        self.initSerial()
+       
 
-        self.serialPort = QSerialPort(self)
-        self.serialPort.setBaudRate(QSerialPort.Baud9600)
-        self.serialPort.setPortName("/dev/ttyACM0")
+import serial
+import time
 
-        self.BtnPressTest.clicked.connect(self.BtnWithArd)
-        
-    def BtnWithArd(self):
-        if not self.serialPort.isOpen():
-            if not self.serialPort.open(QIODevice.ReadWrite):
-                print("Failed to open serial port")
-                return
-        self.serialPort.write(b'button_pressed\n')
+def main():
+    port = "/dev/ttyACM0" 
+    baudrate = 9600
+    ser = serial.Serial(port, baudrate, timeout=1)
 
+    button_count = []  
 
+    try:
+        while True:
+            if ser.inWaiting() > 0:
+                line = ser.readline().decode('utf-8').rstrip()
+                if line.isdigit():  
+                    button_count.append(int(line))
+                    # print("Total button presses:", button_count)
+    except KeyboardInterrupt:
+        print("Program stopped")
+    finally:
+        ser.close()
 
-if __name__ == "__main__" :
-    app = QApplication(sys.argv)
-    myWindows = WindowClass()
-    myWindows.show()
+if __name__ == "__main__":
+    main()
 
-    sys.exit(app.exec_())
