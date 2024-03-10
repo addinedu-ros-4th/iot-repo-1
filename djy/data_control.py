848eab579dab7d5ca9395a06c3930edc40056d7d
yhryu96 <yhryu96@gmail.com>
2024-03-10 12:14:20 +0900

    first connection test with json

--- djy/data_control.py
+++ djy/data_control.py
@@ -1,27 +1,67 @@
 import socket
-"""
-pc에서 esp32가 보내는 메세지를 받기 위한 코드.
-파이썬의 소켓 모듈을 사용하여 데이터를 주고 받습니다.
-"""
+from iot_database import Database
+
+import socket
+import threading
+import json
+from iot_database import Database
+
+def handle_client(client_socket):
+    try:
+        while True:
+            data = client_socket.recv(1024)
+            if not data:
+                break
+
+            try:
+                data_dict = json.loads(data.decode('utf-8'))
+
+                if "selected_date" in data_dict:  # Qt data
+                    process_db_query(data_dict, client_socket)
+
+            except json.JSONDecodeError:
+                # if not JSON, ESP32 data
+                process_sensor_data_from_esp(data.decode('utf-8'))
+
+    finally:
+        client_socket.close()
+
+def process_db_query(data_dict, client_socket):
+    iot_db = Database("iot-project.czcywiaew4o2.ap-northeast-2.rds.amazonaws.com", 3306, "admin", "qwer1234", "iot_project")
+    iot_db.connect()
+    df = iot_db.watch_log(data_dict["selected_date"])
+    df_json = df.to_json(orient='records')
+
+    with open("server_data.json", "w") as file:
+        file.write(df_json)
+
+    client_socket.sendall(df_json.encode('utf-8'))
+
+# JSON type sensor data(option)
+def process_sensor_data(data_dict):
+   pass
 
 
-host = '0.0.0.0'  # ESP32의 IP 주소
-port = 8080  # 포트 번호
+def process_sensor_data_from_esp(data_str):
 
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #소켓을 만들어 줍니다.
-server_socket.bind((host, port)) #esp32와 연결
-server_socket.listen(5) #esp32에서 데이터 보내는 걸 기다립니다. 
+    print(f"Received data from ESP32: {data_str}")
 
-print(f"Server is listening on {host}:{port}")
+def start_server():
+    host = '172.30.1.50'
+    port = 8080
 
-while True:
-    client_socket, addr = server_socket.accept() # esp32의 주소와 어떤 값을 받아옴.  
-    print(f"Connection from {addr}") # 받아온 값과 ip주소가 출력
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind((host, port))
+    server_socket.listen(5)
+    print(f"Server is listening on {host}:{port}")
 
-    data = client_socket.recv(1024) #esp에서 보내는 데이터를 저장하는 변수
-    if data:
-        print(f"Received data: {data.decode('utf-8')}") #데이터 출력
-    else:
-        print("connection end")
+    while True:
+        client_socket, addr = server_socket.accept()
+        print(f"Connection from {addr}")
+        client_thread = threading.Thread(target=handle_client, args=(client_socket,))
+        client_thread.start()
 
-    client_socket.close()
+if __name__ == "__main__":
+    start_server()
+
+
