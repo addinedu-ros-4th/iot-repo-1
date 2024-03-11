1d8f75a989b86bbd42714cc191e52fcdad744f81
IMddubin <lsv2620@gmail.com>
2024-03-11 15:23:00 +0900

    modify farm1, 2

--- src/arduino_ex/arduino_farm1/arduino_farm1.ino
+++ src/arduino_ex/arduino_farm1/arduino_farm1.ino
@@ -1,18 +1,18 @@
 #include <Wire.h> 
 #include <DHT.h> // 온습도센서 라이브러리 
-#include <Servo.h> //Servo 라이브러리
+//#include <Servo.h> //Servo 라이브러리
 
 #define DHTPIN 4 // 온습도센서 핀번호
 #define DHTTYPE DHT11
 #define SOIL_HUMI1 A0
 #define A_1A 9 // 모터드라이버 A_1A 단자 연결 핀번호(워터모터1용)
 #define A_1B 10 // 모터드라이버 A_1B 단자 연결 핀번호
-#define B_1A 5 // 모터드라이버 (환풍미니모터용)
-#define B_1B 6 // 모터드라이버 (환풍미니모터용)
+#define A_2A 5 // 모터드라이버 (환풍미니모터용)
+#define A_2B 6 // 모터드라이버 (환풍미니모터용)
 
 DHT dht(DHTPIN, DHTTYPE); // 온습도센서 사용 설정
 
-Servo servo1; // 환기 서보모터
+//Servo servo1; // 환기 서보모터
 // Servo servo2; // 가습 서보모터
 int soil_humi1, psoil_humi1;
 int echo1 = 8; // 초음파센서1
@@ -28,12 +28,12 @@ void setup() {
  pinMode(A_1B, OUTPUT);  
  digitalWrite(A_1A, LOW); // 모터드라이브 초기값은 끈 상태
  digitalWrite(A_1B, LOW);
- pinMode(B_1A, OUTPUT);
- pinMode(B_1B, OUTPUT);  
- digitalWrite(B_1A, LOW);
- digitalWrite(B_1B, LOW); 
- servo.attach(1); // 환풍기 서보모터
- servo.attach(2); // 가습기 서보모터
+ pinMode(A_2A, OUTPUT);
+ pinMode(A_2B, OUTPUT);  
+ digitalWrite(A_2A, LOW);
+ digitalWrite(A_2B, LOW); 
+ //servo.attach(1); // 환풍기 서보모터
+ //servo.attach(2); // 가습기 서보모터
 }
 
 void loop() {  
@@ -76,8 +76,8 @@ void loop() {
   Serial.println("화분1 물공급!");
   digitalWrite(A_1A, LOW); // 워터펌프 중단
   digitalWrite(A_1B, LOW);
-  digitalWrite(B_1A, LOW); // 워터펌프가 동작하는 동안 미니모터는 중지, 미니모터와 워터펌프가 동시 작동 시 전압 부족 현상 막음 
-  digitalWrite(B_1B, LOW); 
+  digitalWrite(A_2A, LOW); // 워터펌프가 동작하는 동안 미니모터는 중지, 미니모터와 워터펌프가 동시 작동 시 전압 부족 현상 막음 
+  digitalWrite(A_2B, LOW); 
   }
  else{  // 그 외 토양수분값이 측정되면 워터모터를 꺼라
   digitalWrite(A_1A, LOW);
@@ -86,21 +86,21 @@ void loop() {
  delay(1000);
  
  if(air_humi > 60){
-  servo.write(90.0);
-  analogWrite(B_1A, 200); // 값을 변화(0~255)시키면서 팬의 세기를 설정
-  digitalWrite(B_1B, LOW);
+  //servo.write(90.0);
+  analogWrite(A_2A, 200); // 값을 변화(0~255)시키면서 팬의 세기를 설정
+  digitalWrite(A_2B, LOW);
   delay(10000); 
-  digitalWrite(B_1A, LOW); // 값을 변화(0~255)시키면서 팬의 세기를 설정
-  digitalWrite(B_1B, LOW);  
+  digitalWrite(A_2A, LOW); // 값을 변화(0~255)시키면서 팬의 세기를 설정
+  digitalWrite(A_2B, LOW);  
   digitalWrite(A_1A, LOW);
   digitalWrite(A_1B, LOW);
   digitalWrite(A_2A, LOW);
   digitalWrite(A_2B, LOW);
   }
  else{
-  servo.write(175.0); 
-  digitalWrite(B_1A, LOW);
-  digitalWrite(B_1B, LOW);
+  //servo.write(175.0); 
+  digitalWrite(A_2A, LOW);
+  digitalWrite(A_2B, LOW);
   }
  delay(2000); 
 }
