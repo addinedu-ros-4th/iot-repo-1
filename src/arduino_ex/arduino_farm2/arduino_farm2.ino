fa4964e9b88be62df3ea01eb2aae580f431f2581
djy0404 <dydrltk1919@naver.com>
2024-03-12 19:22:16 +0900

    ardu2 modified

--- src/arduino_ex/arduino_farm2/arduino_farm2.ino
+++ src/arduino_ex/arduino_farm2/arduino_farm2.ino
@@ -74,13 +74,14 @@ void loop() {
    if (!error) {
      String ledCommand = recvDoc["ledCommand"];
      String soilwaterCommand = recvDoc["soilwaterCommand"];
-     
+     String wtankCommand = recvDoc["wtankCommand"];
+     String humitankCommand = recvDoc["humitankCommand"];
      
 
-     if (soilwaterCommand == "on") {
+     if (soilwaterCommand == "on" && wtankCommand == "on") {
        analogWrite(B_1A, 220); // LED 켜기
        digitalWrite(B_2B,LOW);
-     } else if (soilwaterCommand == "off") {
+     } else if (soilwaterCommand == "off" || wtankCommand =="off") {
        digitalWrite(B_1A, LOW); // LED 끄기
        digitalWrite(B_2B, LOW);
      }
@@ -91,6 +92,8 @@ void loop() {
      else if (ledCommand == "off") {
       digitalWrite(ledpin,LOW);
      }
+
+     
 
     
    }
